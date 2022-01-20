f6ec1ce00cd24e653de1f422a9f5985a2030e815
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-01-20 06:19:47 +0500

    loops

---
+++ BasicOperators.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+num1=12
+num2=11
+
+echo "Sum = " `expr $num1 + $num2`
+echo "Diff = " `expr $num1 - $num2`
+echo "Product = " `expr $num1 \* $num2`
+echo "Quotient = " `expr $num1 / $num2`
---
+++ forLoop.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+for var in 0 1 2 3 4 5 9
+do
+    echo $var
+done
